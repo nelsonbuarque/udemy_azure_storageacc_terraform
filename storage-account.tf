4c7563fa7f59b71657f0f9a9eb705c9dc7a72458
Nelson <nelson_buarque@hotmail.com>
2022-04-18 09:31:34 -0300

    Update storage-account.tf

---
+++ storage-account.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "first_resource_group" {
+  name     = "rg-first_storage_account_udemy"
+  location = var.location
+  tags = ""
+
+}
+
+resource "azurerm_storage_account" "fist_storage_account" {
+  name                     = "nelson"
+  resource_group_name      = azurerm_resource_group.first_resource_group.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    environment = ""
+  }
+}
+
+resource "azurerm_storage_container" "first_container" {
+  name                  = ""
+  storage_account_name  = azurerm_storage_account.fist_storage_account.name
+}
