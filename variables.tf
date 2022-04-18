1525ae34bd45c022512a4d2e2d791c914432887f
Nelson <nelson_buarque@hotmail.com>
2022-04-18 11:09:23 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "Variavel que indica a regiao onde os recursos vão ser criados"
-  type = string
-  default = "East US 2"
+  type        = string
+  default     = "East US 2"
   #Sobrescrever = "export TF_VAR_location="Brazil South"
   #or
   #terraform plan -var='location-Brazil South'
@@ -11,14 +11,14 @@ variable "location" {
 
 variable "account_tier" {
   description = "Tier da Stirage Acc na Azure"
-  type = string
-  default = "Standard"
+  type        = string
+  default     = "Standard"
 }
 
 variable "account_replication_type" {
   description = "Tipo de replicação de dados da Storage Account"
-  type = string
-  default = "LRS"
+  type        = string
+  default     = "LRS"
   #Tornar o valor da variavel sencivel, não mostrar o valor dela no plan
   #sensitive = true
 }
